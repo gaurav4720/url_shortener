908f74783a0069a0c550269ffa596592c28493d6
gaurav4720 <gauravgupta4720@gmail.com>
2024-07-14 14:04:31 +0530

    bug fixes

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -5,11 +5,13 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     if (!req.user) res.redirect('/login');
-    const allUrls = await URL.find({ createdBy: req.user._id});
+    else {
+        const allUrls = await URL.find({ createdBy: req.user._id });
 
-    return res.render("home", {
-        urls: allUrls
-    });
+        return res.render("home", {
+            urls: allUrls
+        });
+    }
 })
 
 router.get("/signup", (req, res) => {
